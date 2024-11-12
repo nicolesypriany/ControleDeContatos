b2f1f97011568fa7cadd658dd9790d1e11c9e4e3
Nicole Sypriany <nicolesypriany@gmail.com>
2024-11-11 22:16:22 -0300

    Add project files.

---
+++ ControleDeContatos/Repositorio/ContatoRepositorio.cs
@@ -0,0 +1,27 @@
+﻿using ControleDeContatos.Data;
+using ControleDeContatos.Models;
+using Microsoft.IdentityModel.Tokens;
+
+namespace ControleDeContatos.Repositorio
+{
+    public class ContatoRepositorio : IContatoRepositorio
+    {
+        private readonly BancoContext _bancoContext;
+        
+        public ContatoRepositorio(BancoContext bancoContext)
+        {
+            _bancoContext = bancoContext;
+        }
+        public List<ContatoModel> BuscarTodos()
+        {
+            return _bancoContext.Contatos.ToList();
+        }
+
+        public ContatoModel Adicionar(ContatoModel contato)
+        {
+            _bancoContext.Contatos.Add(contato);
+            _bancoContext.SaveChanges();
+            return contato;
+        }
+    }
+}
